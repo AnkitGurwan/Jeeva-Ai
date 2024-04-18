9e10fb15b1ac843f26c5b8553121a5dc40f5a415
AnkitGurwan <ankitgurwan083@gmail.com>
2024-04-19 00:34:50 +0530

    made design responsive

--- frontend/src/Components/allForms.js
+++ frontend/src/Components/allForms.js
@@ -36,18 +36,19 @@ const FormTable = () => {
     },[])
 
   return (
-    <div className='mt-8 mb-4 mx-[2%] md:mx-[5%] p-5 shadow-md rounded-lg border text-[0.9rem]' style={{fontFamily: "Monospace"}}>
+    <div className='w-full md:w-auto md:mx-[5%] mt-8 mb-4 p-2 md:p-5 shadow-md rounded-lg border text-[0.9rem]' style={{fontFamily: "Monospace"}}>
       <h2 className='mb-4 p-2 rounded-sm text-gray-600 bg-gray-200 w-fit'>Sound Recording Data</h2>
-      <table className='w-full'>
+      <div className='overflow-x-scroll md:overflow-hidden'>
+      <table className='w-[150vw] md:w-full'>
         <thead className='w-full'>
-          <tr className='w-full flex justify-between text-gray-600' >
-            <th className='border p-2 w-[5%]'>S.No</th>
-            <th className='border p-2 w-[15%]'>Doctor Name</th>
-            <th className='border p-2 w-[15%]'>Patient Name</th>
-            <th className='border p-2 w-[10%]'>Patient Age</th>
-            <th className='border p-2 w-[15%]'>Recording Date</th>
-            <th className='border p-2 w-[30%]'>Audio</th>
-            <th className='border p-2 w-[10%]'>Remove</th>
+          <tr className='w-full flex justify-between text-gray-600 text-xs md:text-lg' >
+            <th className='border flex items-center justify-center p-2 tracking-tight leading-none w-[8%] md:w-[5%]'>S.No</th>
+            <th className='border flex items-center justify-center p-2 tracking-tight leading-none w-[15%]'>Doctor Name</th>
+            <th className='border flex items-center justify-center p-2 tracking-tight leading-none w-[15%]'>Patient Name</th>
+            <th className='border flex items-center justify-center p-2 tracking-tight leading-none w-[10%]'>Patient Age</th>
+            <th className='border flex items-center justify-center p-2 tracking-tight leading-none w-[15%]'>Recording Date</th>
+            <th className='border flex items-center justify-center p-2 tracking-tight leading-none w-[30%]'>Audio</th>
+            <th className='border flex items-center justify-center p-2 tracking-tight leading-none w-[10%]'>Remove</th>
           </tr>
         </thead>
         <div className='w-full'>
@@ -56,12 +57,12 @@ const FormTable = () => {
             ?
             <div>
               {allForms.map((form,index) => (
-                <tr key={form._id} className='w-full flex justify-between items-center h-12 text-gray-600 font-Manrope'>
-                  <div className='border w-[5%] flex items-center justify-center capitalize h-full'>{index + 1}</div>
-                  <div className='border w-[15%] flex justify-center items-center capitalize h-full'>{form.doctorName}</div>
-                  <div className='border w-[15%] flex items-center justify-center capitalize h-full'>{form.patientName}</div>
-                  <div className='border w-[10%] flex items-center justify-center h-full'>{form.patientAge}</div>
-                  <div className='border w-[15%] flex items-center justify-center h-full'>{new Date(form.recordingDate).toLocaleDateString()}</div>
+                <tr key={form._id} className='w-full flex justify-between items-center h-12 text-gray-600 text-xs md:text-lg'>
+                  <div className='border w-[8%] md:w-[5%] p-1 flex items-center justify-center text-center capitalize h-full tracking-tight leading-none'>{index + 1}</div>
+                  <div className='border w-[15%] p-1 flex justify-center items-center text-center capitalize h-full tracking-tight leading-none'>{form.doctorName}</div>
+                  <div className='border w-[15%] flex items-center justify-center text-center capitalize h-full tracking-tight leading-none'>{form.patientName}</div>
+                  <div className='border w-[10%] flex items-center justify-center h-full tracking-tight leading-none'>{form.patientAge}</div>
+                  <div className='border w-[15%] flex items-center justify-center h-full tracking-tight leading-none'>{new Date(form.recordingDate).toLocaleDateString()}</div>
                   <audio
                     className='w-[30%] h-full border py-1 px-2'
                     src={form.soundFile}
@@ -95,6 +96,7 @@ const FormTable = () => {
               </div>)}
         </div>
       </table>
+      </div>
     </div>
   );
 };
--- frontend/src/Components/header.js
+++ frontend/src/Components/header.js
@@ -3,12 +3,12 @@ import { FaRegUser } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className="bg-gray-900 bg-opacity-80 text-white px-8 py-4 flex justify-between items-center">
-        <div className='text-xl font-bold flex items-center gap-3'>
-            <img className='w-10 h-10' src="https://static.wixstatic.com/media/8ae22a_7e8887b754df4dfea093c4e73aeaaaa2~mv2.jpg/v1/fill/w_134,h_126,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Screenshot%202023-09-23%20212141_edited.jpg" alt='jeeva-ai-logo' />
+    <header className="bg-gray-900 bg-opacity-80 text-white px-4 md:px-8 py-4 flex justify-between items-center">
+        <div className='text-xl font-bold flex items-center gap-2 md:gap-3'>
+            <img className='w-9 md:w-10 h-9 md:h-10' src="https://static.wixstatic.com/media/8ae22a_7e8887b754df4dfea093c4e73aeaaaa2~mv2.jpg/v1/fill/w_134,h_126,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Screenshot%202023-09-23%20212141_edited.jpg" alt='jeeva-ai-logo' />
             <div>AI-Based Diagnosis</div>
         </div>
-        <div className='text-lg font-medium flex items-center gap-2'>
+        <div className='text-lg font-medium flex items-center gap-1 md:gap-2'>
           <FaRegUser/>
           <div>Guest</div>
         </div>
